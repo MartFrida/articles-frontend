f2daaa1d48c3956c90375dc4b6422605f0547731
Mary <32392607+MartFrida@users.noreply.github.com>
2025-01-18 11:32:31 +0100

    fix button read more/less in article item

--- src/pages/Articles/ArticleItem.jsx
+++ src/pages/Articles/ArticleItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { cutText } from '../../helpers/cutText'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteArticleThunk } from '../../redux/articles/operations'
@@ -8,6 +8,7 @@ import { formatDistanceToNow } from 'date-fns'
 export const ArticleItem = ({ _id, title, description, author, createdAt }) => {
 	const dispatch = useDispatch()
 	const user = useSelector(selectUserName)
+	const [isCutDescription, setIsCutDescription] = useState(true)
 	return (
 		<div className='w-[90%] mt-2 mx-auto p-4 border-2 border-black rounded-md shadow-md'>
 			<h2 className='text-4xl font-bold'>{title}</h2>
@@ -15,7 +16,7 @@ export const ArticleItem = ({ _id, title, description, author, createdAt }) => {
 			<div className='flex gap-4'>
 			</div>
 			<p className='font-light'>{formatDistanceToNow(createdAt, { addSuffix: true })}</p>
-			<p className='text-xl'>{cutText(description)}</p>
+			<p className='text-xl first-letter:text-7xl'> {isCutDescription ? cutText(description) : description} </p>
 			<div className='flex gap-4 py-2 mt-4 justify-end'>
 				{user === author && (
 					<>
@@ -28,7 +29,16 @@ export const ArticleItem = ({ _id, title, description, author, createdAt }) => {
 						</button>
 					</>
 				)}
-				<button className='border-2 border-black rounded-md px-4 py-1 shadow-md'>read more</button>
+				{isCutDescription ?
+					<button onClick={() => setIsCutDescription(false)} className='relative'>
+						<span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-800 "></span>
+						<span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-600 hover:text-white" >read more</span>
+					</button> :
+					<button onClick={() => setIsCutDescription(true)} className='relative'>
+						<span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-800 "></span>
+						<span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-600 hover:text-white" >read less</span>
+					</button>
+				}
 			</div>
 		</div>
 	)
